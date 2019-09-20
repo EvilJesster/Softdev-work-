a75c9ba6186500529fadd7f0b9411a97b072b6c4
EvilJesster <jhall00@stuy.edu>
2019-09-20 08:29:21 -0400

    post pratham changes

--- 03_csv/dothisthing.py
+++ 03_csv/dothisthing.py
@@ -5,17 +5,19 @@ with open('occupations.csv', mode='r') as target:
     next(reader)
     holddict = {}
     for row in reader:
-        holddict[row[0]] = (float(row[1]) * 10)
+        holddict[row[0]] = (float(row[1]))
 heavylist = []
+keys = []
 for i in holddict:
     if(i != 'Total'):
-        holdnum = (holddict[i])
-        while(holdnum > 0):
-            heavylist.append(i)
-            holdnum -= 1
+        heavylist.append(holddict[i])
+        keys.append(i)
+    #    while(holdnum > 0):
+    #         heavylist.append(i)
+    #         holdnum -= 1
 
 
 
-print(len(heavylist))
-print(random.choice(heavylist))
+
+print(random.choices(keys, heavylist, k=1))
 print(holddict)
